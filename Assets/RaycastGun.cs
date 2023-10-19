e2b64701db071f4b3227f10a4fc143dbfcce9b9d
Ariel454 <ariel.raura@udla.edu.ec>
2023-10-19 02:26:44 -0500

    ScoreUpdated, and Script Complete

--- Assets/RaycastGun.cs
+++ Assets/RaycastGun.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 [RequireComponent(typeof(LineRenderer))]
 public class RaycastGun : MonoBehaviour
 {
     public Camera playerCamera;
     public Transform laserOrigin;
+    public TMPro.TextMeshProUGUI contadorTexto;
     public float gunRange = 50f;
     public float fireRate = 0.2f;
     public float laserDuration = 0.05f;
     private int shotsHit = 0;
+    private int cubosDestruidos = 0;
+    private int esferasDestruidas = 0;
+    private string textoContador;
+
 
     LineRenderer laserLine;
     float fireTimer;
@@ -29,16 +36,49 @@ public class RaycastGun : MonoBehaviour
             laserLine.SetPosition(0, laserOrigin.position);
             Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hit;
-            if(Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
+            if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
             {
                 laserLine.SetPosition(1, hit.point);
-                shotsHit++;
 
-                if (shotsHit >= 3)
+                // Comprueba si el objeto impactado tiene la etiqueta "Terreno"
+                if (hit.transform.CompareTag("Terreno"))
                 {
-                    Destroy(hit.transform.gameObject);
-                    shotsHit = 0;  // Reinicia el contador después de destruir el objeto
+                    // Ignora el terreno y no hace nada
                 }
+                else if (hit.transform.CompareTag("Cubo"))
+                {
+                    // Si es un cubo, se destruye con 2 disparos
+                    shotsHit++;
+                    if (shotsHit >= 2)
+                    {
+                        Destroy(hit.transform.gameObject);
+                        shotsHit = 0;
+                            cubosDestruidos++;
+                    }
+                }
+                else if (hit.transform.CompareTag("Esfera"))
+                {
+                    // Si es una esfera, se destruye con 3 disparos
+                    shotsHit++;
+                    if (shotsHit >= 3)
+                    {
+                        Destroy(hit.transform.gameObject);
+                        shotsHit = 0;
+                            esferasDestruidas++;
+                    }
+                }
+                else
+                {
+                    // Si no tiene ninguna etiqueta, se destruye con 1 disparo
+                    shotsHit++;
+                    if (shotsHit >= 1)
+                    {
+                        Destroy(hit.transform.gameObject);
+                        shotsHit = 0;
+                    }
+                }
+                textoContador = "Cubos destruidos: " + cubosDestruidos + "\nEsferas destruidas: " + esferasDestruidas;
+                contadorTexto.text = textoContador;
             }
             else
             {
